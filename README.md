33368bc08b4226d3d13c7b289081dd25522246e7
Glenio Pimentel <GlenioSP@users.noreply.github.com>
2023-08-13 12:41:17 +0000

    Add file `.editorconfig` and scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementation of the https://tabnews.com.br/ website for the https://curso.dev/ course
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>This is the first Clone TabNews page!</h1>
+  return <h1>This is the first Clone TabNews page!</h1>;
 }
 
 export default Home;
