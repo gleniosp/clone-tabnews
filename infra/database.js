0e79186708cc0488c4a8a4d39a66decc5ff7ae00
Glenio Pimentel <GlenioSP@users.noreply.github.com>
2025-01-04 14:15:05 +0000

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n Error within the database.js catch statement");
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
