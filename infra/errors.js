09db2d27218aec75a3287d615dd42ac6aa6fe8d1
Glenio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-01-24 10:56:11 +0000

    refactor: standardize controllers with `next-connect`

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,52 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("An unexpected internal error has happened", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Contact the support team";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  // overwrites the JSON method from Error
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service is currently unavailable", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verify if the service is currently available";
+    this.statusCode = 503;
+  }
+
+  // overwrites the JSON method from Error
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Not allowed method for this endpoint");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verify if the provided HTTP method is valid for this endpoint";
+    this.statusCode = 405;
   }
 
   // overwrites the JSON method from Error
