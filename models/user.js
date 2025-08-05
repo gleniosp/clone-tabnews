502eb8bb635f81b253a658d8163c8fddc0402e41
gleniosp <11273108+gleniosp@users.noreply.github.com>
2025-08-05 18:44:43 +0000

    feat: implement  endpoint with session renewal

--- models/user.js
+++ models/user.js
@@ -2,6 +2,37 @@ import database from "infra/database.js";
 import password from "models/password.js";
 import { NotFoundError, ValidationError } from "infra/errors.js";
 
+async function findOneById(id) {
+  const userFound = await runSelectQuery(id);
+
+  return userFound;
+
+  async function runSelectQuery(id) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          * 
+        FROM
+          users
+        WHERE
+          id = $1
+        LIMIT
+          1
+        ;`,
+      values: [id],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "The provided id wasn't found.",
+        action: "Verify if the id is properly typed.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
 
@@ -130,7 +161,7 @@ async function update(username, userInputValues) {
           id = $1
         RETURNING
           *
-      `,
+      ;`,
       values: [
         userWithNewValues.id,
         userWithNewValues.username,
@@ -192,6 +223,7 @@ async function hashPasswordInObject(userInputValues) {
 
 const user = {
   create,
+  findOneById,
   findOneByUsername,
   findOneByEmail,
   update,
