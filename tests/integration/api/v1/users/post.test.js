bfbc758df0e6a373254e5ee0e3c5049ab27eb6da
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-11-13 15:47:32 +0000

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -135,4 +135,37 @@ describe("POST /api/v1/users", () => {
       });
     });
   });
+
+  describe("Default user", () => {
+    test("With unique and valid data", async () => {
+      // a logged user tries to create another user
+      const user1 = await orchestrator.createUser();
+      await orchestrator.activateUser(user1);
+      const user1SessionObject = await orchestrator.createSession(user1.id);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${user1SessionObject.token}`,
+        },
+        body: JSON.stringify({
+          username: "loggeduser",
+          email: "loggeduser@example.com",
+          password: "password123",
+        }),
+      });
+
+      expect(user2Response.status).toBe(403);
+
+      const user2ResponseBody = await user2Response.json();
+
+      expect(user2ResponseBody).toEqual({
+        name: "ForbiddenError",
+        message: "You don't have permission to execute this action.",
+        action: 'Verify if your user has the "create:user" feature',
+        status_code: 403,
+      });
+    });
+  });
 });
