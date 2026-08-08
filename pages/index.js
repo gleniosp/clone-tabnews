e832d17b63435eb07d38b110344134a9991c6403
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2026-08-08 11:27:58 +0000

    feat: use `DefaultLayout` on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import DefaultLayout from "interface/DefaultLayout";
+
 function Home() {
-  return <h1>This is the first Clone TabNews page!</h1>;
+  return (
+    <DefaultLayout
+      metadata={{
+        description: "tech content for developers",
+      }}
+    >
+      <h1>Tech content for developers.</h1>
+    </DefaultLayout>
+  );
 }
 
 export default Home;
